da6eeaaca5fead09e3a2e3c2f27b088ca77c65bb
Sarah Hutchins <shutchin@thoughtworks.com>
2015-03-12 15:23:37 -0500

    Sarah: Extracted file parsing logic since it was being used for both files

--- finder.py
+++ finder.py
@@ -2,27 +2,26 @@
 
 import sys
 
+def create_list_from_file(file, separator):
+  new_list = []
+  with open(file, "r") as ins:
+    for line in ins:
+      split = line.split(separator)
+      if len(split) > 1:
+        new_list.append(split[1])
+  return new_list
+
 if len(sys.argv) != 3:
   print len(sys.argv)
   print sys.argv
   sys.exit("Usage: python finder.py <potentialAdditionalList> <knownItemsList>")
 
-errorCodes = []
-with open(sys.argv[1], "r") as ins:
-  for line in ins:
-    split = line.split("|")
-    if len(split) > 1:
-      errorCodes.append(split[1])
+errorCodes = create_list_from_file(sys.argv[1], "|")
 
-knownErrorCodes = []
-with open(sys.argv[2]) as ins:
-  for line in ins:
-    split = line.split(",")
-    if len(split) > 1:
-      knownErrorCodes.append(split[1])
+knownErrorCodes = create_list_from_file(sys.argv[2], ",")
   
 unknownErrorCodes = list(set(errorCodes) - set(knownErrorCodes))
 
-print "%s error codes found" % len(unknownErrorCodes)
+print "%s new items found" % len(unknownErrorCodes)
 for error in unknownErrorCodes:
   print error
